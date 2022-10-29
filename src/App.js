2b900d18e67242b269f9419e862ca82bdd1c0f90
Gopichand785 <bgopichand3@gmail.com>
2022-10-29 18:57:39 +0530

    k

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'
+import './App.css'
+import Header from './components/Header'
+import Custum from './components/Custum'
+import Details from './components/Details'
+import Teams from './components/Teams'
+import Pricing from './components/Pricing'
+import Footer from './components/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  return(
+    <>
+      <Navbar/>
+      <Header/>
+      <Custum/>
+      <Details/>
+      <Teams/>
+      <Pricing/>
+      <Footer/>
+    </>
+  )
 }
-
-export default App;
